93e3834f9549180d8b15cf69d57932d3de6e1944
GuiDDuran <guilherme.d.gea@gmail.com>
2024-03-14 07:30:31 -0300

    Integração com bd

--- cadastrocliente/src/main/java/br/com/ibmec/backend/cadastrocliente/controller/ClienteController.java
+++ cadastrocliente/src/main/java/br/com/ibmec/backend/cadastrocliente/controller/ClienteController.java
@@ -1,7 +1,9 @@
 package br.com.ibmec.backend.cadastrocliente.controller;
 
 import br.com.ibmec.backend.cadastrocliente.model.Cliente;
+import br.com.ibmec.backend.cadastrocliente.repository.ClienteRepository;
 import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
@@ -14,7 +16,9 @@ import java.util.List;
 @RequestMapping("/cliente") // É utilizado para chamar o controller cliente, também irá aparecer na url
 public class ClienteController {
 
-    private static ArrayList<Cliente> repositorio = new ArrayList<>(); // Cria um array cliente.
+    //private static ArrayList<Cliente> repositorio = new ArrayList<>(); // Cria um array cliente.
+    @Autowired
+    private ClienteRepository repositorio;
 
     @GetMapping("/listar")  // O get é utilizado para exibir a página ao usuário e o /listar será o endereço da url
     public String listarCliente(Model model) {
@@ -26,7 +30,7 @@ public class ClienteController {
         cliente1.setDataNascimento("20/12/2004");
         repositorio.add(cliente1);*/
 
-        model.addAttribute("listaCliente", repositorio);  // Liga os dados da model com a página html.
+        model.addAttribute("listaCliente", repositorio.findAll());  // Liga os dados da model com a página html.
 
         return "listar-cliente";  // Vai retornar a página html listar-cliente dentro da pasta resources/templates.
     }
@@ -41,33 +45,42 @@ public class ClienteController {
         if (result.hasErrors()){
             return "adicionar-cliente";
         }
+        repositorio.save(cliente);
+        return "redirect:/cliente/listar";
 
-        int id = 1;
+        /*int id = 1;
         if (repositorio.size() > 0) {
             Cliente ultimo = repositorio.get(repositorio.size() - 1);
             id = ultimo.getId() + 1;
         }
         cliente.setId(id);
-
         repositorio.add(cliente);
-        return "redirect:/cliente/listar";  // Após adicionar o cliente, ele é direcionado para a página listar.
+        return "redirect:/cliente/listar";  // Após adicionar o cliente, ele é direcionado para a página listar.*/
     }
 
     @GetMapping("/excluir/{id}")
     public String excluir(@PathVariable("id") int id) {
-        Cliente clienteASerExcluido = null;
+        Cliente clienteASerExcluido = repositorio.getById(id);
+        repositorio.delete(clienteASerExcluido);
+        return "redirect:/cliente/listar";
+
+        /*Cliente clienteASerExcluido = null;
         for (Cliente item : repositorio){
             if (item.getId() == id) {
                 clienteASerExcluido = item;
             }
         }
         repositorio.remove(clienteASerExcluido);
-        return "redirect:/cliente/listar";
+        return "redirect:/cliente/listar";*/
     }
 
     @GetMapping("/editar/{id}")
     public String editar(@PathVariable("id") int id, Model model) {
-        Cliente clienteASerEditado = null;
+        Cliente clienteASerEditado = repositorio.getById(id);
+        model.addAttribute("cliente", clienteASerEditado);
+        return "editar-cliente";
+
+        /*Cliente clienteASerEditado = null;
         for (Cliente item : repositorio) {
             if (item.getId() == id){
                 clienteASerEditado = item;
@@ -75,7 +88,7 @@ public class ClienteController {
             }
         }
         model.addAttribute("cliente", clienteASerEditado);
-        return "editar-cliente";
+        return "editar-cliente";*/
 
         /*Cliente cliente = repositorio.stream()
                 .filter(c -> c.getId() == id)
@@ -87,13 +100,16 @@ public class ClienteController {
 
     @PostMapping("/atualizar/{id}")
     public String atualizar(@PathVariable("id") int id, Cliente newData) {
-        Cliente clienteASerEditado = null;
+        Cliente clienteASerEditado = repositorio.getById(id);
+        
+        /*Cliente clienteASerEditado = null;
         for (Cliente item : repositorio) {
             if (item.getId() == id) {
                 clienteASerEditado = item;
                 break;
             }
-        }
+        }*/
+
         clienteASerEditado.setNome(newData.getNome());
         clienteASerEditado.setCpf(newData.getCpf());
         clienteASerEditado.setEmail(newData.getEmail());
--- cadastrocliente/src/main/java/br/com/ibmec/backend/cadastrocliente/model/Cliente.java
+++ cadastrocliente/src/main/java/br/com/ibmec/backend/cadastrocliente/model/Cliente.java
@@ -1,23 +1,32 @@
 package br.com.ibmec.backend.cadastrocliente.model;
 
+import jakarta.persistence.*;
 import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import lombok.Data;
 
 @Data
+@Entity
 public class Cliente {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
     private int id;
 
+    @Column
     @NotBlank(message = "Campo obrigatório")
     private String nome;
 
+    @Column
     @NotBlank(message = "Campo obrigatório")
     @Email(message = "Formato incorreto")
     private String email;
 
+    @Column
     @NotBlank(message = "Campo obrigatório")
     private String cpf;
 
+    @Column
     @NotBlank(message = "Campo obrigatório")
     private String dataNascimento;
 }
